be796860c80d2329e8efd297b35d933f38685ffa
kris-na <67404646+kris-na@users.noreply.github.com>
2020-06-27 23:48:20 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,35 @@
+// krish 25th june 2020, https://github.com/AJLoveChina/birthday
+var config = {
+    // write any no ofsentence
+    // lenth <15 words
+    texts: [
+        "To the ",      // comma at end of every sentence
+        "Sweetest girl I know",  // same as above
+        " 🎂🎉🎁 ", 
+        "I wish you ",
+        "more years of a sweet & happy life 😇 ",
+        
+        "🍭🍬🍭🍬 ",
+        "🎊HAPPY BIRTHDAY🎊 🎊SHWETA🎊",
+    ],
+    /**
+    need images
+     */
+    imgs: {
+        "Sweetest girl I know":"./imgs/xiaokeai.png" ,
+        "I wish you " : "./imgs/1.jpeg" ,
+        "more years of a sweet & happy life 😇 ": "./imgs/2.jpeg",
+        "🎊HAPPY BIRTHDAY🎊 🎊SHWETA🎊": "./imgs/3.gif" ,
+    },
+    // button description
+    desc: {
+        turn_on: "lights on 💡",
+        play: "play music 🎸",
+        bannar_coming: "decoration 🎉",
+        balloons_flying: "something missing??",
+        cake_fadein: " Cake 🎂 ？",
+        light_candle: "candle ? ",
+        wish_message: "happy birthday 🎁",
+        story: "A MESSAGE FOR YOU ",
+    }
+};
